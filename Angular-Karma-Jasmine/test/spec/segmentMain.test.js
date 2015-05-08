2976a5153c89e7c630daa532cf56e1c9429cdc96
Rajesh Babu <trcrajesh_be@yahoo.com>
2015-05-08 15:23:52 -0400

    Update segmentMain.test.js

--- Angular-Karma-Jasmine/test/spec/segmentMain.test.js
+++ Angular-Karma-Jasmine/test/spec/segmentMain.test.js
@@ -5,7 +5,7 @@ describe('Segmentation Unit Testing', function() {
 	var createSegmentCtrl2,
 	scope;
 
-	beforeEach(inject(function ($rootScope, $controller) {
+	beforeEach(inject(function ($rootScope, $controller, $http) {
 		 var html = '<input id="segmentsListFw" value=""></input>';
          angular.element(document.body).append(html);
 		scope = $rootScope.$new();
@@ -16,6 +16,13 @@ describe('Segmentation Unit Testing', function() {
 	it('Checks Query format extract', function () {
 		var idArr= [{id:"1"},{id:"2"}];
 		expect(createSegmentCtrl2.getSelectedSegmentsIds(idArr)).toEqual("?id=1,2");
+	});
+	it('Checks Add Remove array elements', function () {
+		var srcArr = [{id:"1"},{id:"2"}],
+			selectedArr = [{id:"1"}],
+			destArr = [];
+		createSegmentCtrl2.addRemoveArr(selectedArr, srcArr, destArr);
+		expect(srcArr.length).toBe(1);
 	});
 
 });
